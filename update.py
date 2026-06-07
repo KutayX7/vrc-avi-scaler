4d285c8d6c4ef0ef6c8cfa77d7ac69dd8162b6c6
KutayX7 <103666634+KutayX7@users.noreply.github.com>
2026-06-07 14:21:35 +0300

    main branch prompt
    It will now prompt you to switch to the main branch if your branch has been deleted. (Using the `-y` argument will auto accept this!)

--- update.py
+++ update.py
@@ -3,7 +3,9 @@ import os
 import argparse
 
 CURRENT: str = "current"
+MAIN: str = "main"
 branch: str = CURRENT
+yes = False
 
 def _discard_unstaged() -> int:
     print("[INFO] Discarding unstaged changes...")
@@ -47,6 +49,14 @@ def _checkout(branch: str) -> int:
         print(f"[ERROR] Failed to checkout the {branch} branch. Return code:", returncode)
     return returncode
 
+def _change_branch(branch_name: str) -> int:
+    global branch
+    branch = branch_name
+    return _checkout(branch)
+
+def _change_branch_and_pull(branch: str) -> int:
+    return _change_branch(branch) or _pull() or 0
+
 def _pull() -> int:
     print("[INFO] Pulling...")
     returncode: int = subprocess.run(["git", "pull"]).returncode
@@ -54,6 +64,14 @@ def _pull() -> int:
         print("[INFO] Pull success.")
     else:
         print("[ERROR] Failed to pull. Return code:", returncode)
+        if branch != MAIN:
+            print("[INFO]: Requested branch may not exist anymore.")
+            if yes:
+                return _change_branch_and_pull(MAIN)
+            else:
+                print(f"Would you like to switch to the {MAIN} branch instead?")
+                if input("y/N > ").strip() == 'y':
+                    return _change_branch_and_pull(MAIN)
     return returncode
 
 def _setup() -> int:
@@ -70,11 +88,10 @@ def _setup() -> int:
         print("[ERROR] Failed to run the setup script. Return code:", returncode)
     return returncode
 
-def force_upgrade(branch: str = CURRENT) -> int:
+def force_upgrade(*, branch: str = CURRENT) -> int:
     return (_discard_current() or
             _fetch() or
-            _checkout(branch) or
-            _pull() or
+            _change_branch_and_pull(branch) or
             _setup() or
             0)
 
@@ -96,6 +113,7 @@ if __name__ == "__main__":
     parser.add_argument("-y", "--yes", default=False)
     args = parser.parse_args()
     branch = args.branch
+    yes = args.yes
 
     if not _validate_branch_name(branch):
         print("[WARNING] Invalid branch name.")
@@ -106,7 +124,7 @@ if __name__ == "__main__":
     print(f"The program will be upgraded to the latest version from the {branch} branch.")
     print( "This only works if you cloned the repository and did not delete the .git folder.")
     print( "This WILL discard your modifications and quite possibly your config, and (re-)install the latest version, even if you're on the latest version already.")
-    if args.yes or input("Please type y and press Enter to continue: ").strip() == "y":
+    if yes or input("Please type y and press Enter to continue: ").strip() == "y":
         if force_upgrade(branch=branch) == 0:
             print("Upgrade success.")
         else:
